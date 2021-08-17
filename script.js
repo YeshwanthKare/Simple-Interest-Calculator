9998d8523f23f8a4afdb90b824c6c8e4a12db77b
YeshwanthKare <yesh.kare@gmail.com>
2021-08-18 02:23:00 +0530

    ADD: added html for Simple Interest

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+window.onload = () => {
+  computeRate();
+  compute();
+};
+
+function computeRate() {
+  let rate = document.getElementById("rate");
+  let span = document.getElementById("rate-span");
+
+  rate.oninput = function () {
+    let rateValue = rate.value * 0.1 + 0.1;
+    let finalRate = rateValue.toFixed(1);
+    span.innerHTML = finalRate + " " + "%";
+    getRate(span.value);
+  };
 }
-        
+
+const getRate = (rateAmount) => {
+  return rateAmount;
+};
+
+function compute(principleAmount) {
+  principleAmount = document.getElementById("principal").value;
+}
+
+const selectYears = (year) => {
+  //   return year;
+  computeSimpleInterest(principle, rate, year);
+};
+
+const computeSimpleInterest = (principle, rate, time) => {
+  console.log({ principle: principle, rate: rate, time: time });
+};
