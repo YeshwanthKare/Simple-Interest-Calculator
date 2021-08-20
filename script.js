409a89d4464104085c3678dffe48a7a58040f5f1
YeshwanthKare <yesh.kare@gmail.com>
2021-08-20 22:26:42 +0530

    ADD: completed the simple interest app

--- script.js
+++ script.js
@@ -1,33 +1,49 @@
-window.onload = () => {
-  computeRate();
-  compute();
-};
-
-function computeRate() {
-  let rate = document.getElementById("rate");
+const getRate = () => {
   let span = document.getElementById("rate-span");
+  let rateValue = rate.value * 0.1 + 0.1;
+  let finalRate = rateValue.toFixed(1);
+  span.innerHTML = finalRate;
 
-  rate.oninput = function () {
-    let rateValue = rate.value * 0.1 + 0.1;
-    let finalRate = rateValue.toFixed(1);
-    span.innerHTML = finalRate + " " + "%";
-    getRate(span.value);
-  };
-}
-
-const getRate = (rateAmount) => {
-  return rateAmount;
+  let currentRate = finalRate;
+  return currentRate;
 };
 
-function compute(principleAmount) {
-  principleAmount = document.getElementById("principal").value;
-}
-
-const selectYears = (year) => {
-  //   return year;
-  computeSimpleInterest(principle, rate, year);
+const getTime = () => {
+  let selectTag = document.getElementById("years").selectedIndex;
+  let options = document.querySelectorAll("option");
+  let years = options[selectTag].value;
+  return years;
 };
 
-const computeSimpleInterest = (principle, rate, time) => {
-  console.log({ principle: principle, rate: rate, time: time });
+const computeSimpleInterest = () => {
+  let principle = document.getElementById("principle").value;
+  let rate = document.getElementById("rate");
+  rate.value = getRate();
+  let time = document.getElementById("years");
+  time.value = getTime();
+
+  let numerator = principle * time.value * rate.value;
+
+  let SI = numerator / 100;
+
+  showResult(principle, time.value, rate.value, SI.toFixed(2));
+};
+
+const showResult = (p, t, r, I) => {
+  let output = document.querySelector(".output");
+
+  output.textContent =
+    "If you deposit " +
+    p +
+    ",<br>" +
+    "at an interest rate of " +
+    r +
+    " %,<br> " +
+    "You will receive an Amount of " +
+    I +
+    ",<br> in the Year 202" +
+    t +
+    ".";
+
+  output.innerHTML = output.textContent;
 };
